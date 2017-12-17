668885064ea4c42929a0d8be33be42c02fe9d3b2
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-17 14:31:32 +1000

    added responsive logo and cleared out some redundant code

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-
+import logo from './images/downer-logo.jpg'
 class App extends Component {
   render() {
     return (
@@ -9,10 +8,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </header>  
       </div>
     );
   }
