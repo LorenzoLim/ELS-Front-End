3258b979506a7d98209f9ad8c90d6df0f4bd98fb
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2017-12-17 15:44:43 +1000

    added bootstrap and set up Sign in component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import logo from './images/downer-logo.jpg'
+import SignIn from './components/Sign-in'
 class App extends Component {
+  state = {
+    login: false
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>  
+          <img src={logo} alt="logo" />
+        <SignIn />
       </div>
+
     );
   }
 }
---
+++ src/components/Sign-in.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import { Form, Label, Input, FormGroup, Button } from 'reactstrap';
+
+ class SignIn extends Component {
+  render() {
+    return(
+      <Form>
+        <FormGroup>
+          <Label for="email">Email</Label>
+          <Input type="email" name="email" id="managerEmail" placeholder="Enter your employee email" />
+        </FormGroup>
+        <FormGroup>
+          <Label for="password">Password</Label>
+          <Input type="password" name="password" id="examplePassword" placeholder="please enter your password" />
+        </FormGroup>
+        <Button color="success">Sign In</Button>
+      </Form>
+    )
+  }
+}
+export default SignIn
