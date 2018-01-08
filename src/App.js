2851d8c3b58f5cc8f74475a08456707f23bf8400
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2018-01-08 15:24:37 +1000

    added sign out button

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import './App.css';
-import logo from './images/downer-logo.jpg'
-import SignIn from './components/Sign-in'
-import CheckIn from './components/Check-in'
+import logo from './images/downer-logo.jpg';
+import SignIn from './components/Sign-in';
+import CheckIn from './components/Check-in';
+import SignOut from './components/Sign-out';
 class App extends Component {
   state = {
     login: false
@@ -11,7 +12,7 @@ class App extends Component {
     return (
       <div className="App">
           <img src={logo} alt="logo" />
-        <CheckIn />
+          <SignOut />
       </div>
 
     );
