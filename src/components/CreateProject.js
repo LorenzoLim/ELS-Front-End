4264c781d7966182a751490890851ca6b519591e
Travis Anderson <31084612+Travis-Anderson83@users.noreply.github.com>
2018-01-12 15:50:51 +1000

    indentation

--- src/components/CreateProject.js
+++ src/components/CreateProject.js
@@ -3,38 +3,38 @@ import { createProject } from '../request'
 
 import {MuiThemeProvider, RaisedButton, TextField} from 'material-ui';
 class SignIn extends Component {
-constructor(props){
-  super(props);
+  constructor(props){
+    super(props);
     this.state = {
       projectNum: '',
       projectLocation: '',
       projectName: '',
       projectStatus: ''
     }
- }
+  }
 
-   handleCreateProject = (event) => {
-     let {projectNum, projectLocation, projectName, projectStatus} = this.state;
-     createProject({
-       method: 'post',
-       url: '/projects',
-       headers: {'Content-Type': 'application/json'},
-       data: {
+  handleCreateProject = (event) => {
+    let {projectNum, projectLocation, projectName, projectStatus} = this.state;
+    createProject({
+      method: 'post',
+      url: '/projects',
+      headers: {'Content-Type': 'application/json'},
+      data: {
         projectNum,
         projectLocation,
         projectName,
         projectStatus
       }
-     })
-     .then((response) => {
-       console.log(response);
-     })
-     .catch((error) => {
-       console.log(error);
-     });
-   }
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  }
 
-render() {
+  render() {
     return (
       <div>
         <MuiThemeProvider>
@@ -65,7 +65,7 @@ render() {
             <br/>
             <RaisedButton className="button" label="Create Project" primary={true} onClick={(event) => this.handleCreateProject(event)} />
           </div>
-         </MuiThemeProvider>
+        </MuiThemeProvider>
       </div>
     );
   }
