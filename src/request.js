97062782bcc4df675b0a1489f6cae94b3771fab8
Chris Hayward <30640240+KingofGondor12@users.noreply.github.com>
2018-01-15 16:05:12 +1000

    Modifed SignIn Component to implement logic

--- src/request.js
+++ src/request.js
@@ -4,4 +4,9 @@ const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL
 })
 
-export {api}
+const setJwt = (token) => {
+  localStorage.setItem('token', token)
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+};
+
+export {api, setJwt}
